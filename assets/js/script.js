cf8caba46f9136fa4ddfbfb06bf07d025ab8b356
Mohamad Almoayad <mohamadalmoayad@modesigns.studio>
2023-04-28 13:46:35 +0200

    still working on modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,41 @@
-let modalsControllers = document.querySelectorAll(".modal-controller");
+const body = document.body;
+const modalDisplay = document.querySelectorAll(".modal-display");
+const modalDismiss = document.querySelectorAll(".modal-dismiss");
+const modal = document.querySelectorAll(".modal");
 
-modalsControllers.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    console.log("eleemnt:", element.dataset?.modalTarget);
-    // event.stopPropagation();
-    console.log("event.target:", event.target);
-    const modalId = event.target.dataset?.modalTarget;
+modalDisplay.forEach((element) => {
+  element.addEventListener("click", (_) => {
+    const modalId = element.dataset?.modalTarget;
     if (!modalId) return;
     const modal = document.getElementById(modalId);
     showModal(modal);
-  },false);
+  });
 });
-// document.addEventListener(
-//   "click",
-//   (event) => {
-//     event.stopPropagation();
-//     console.log("event.target:", event.target);
-//     if (event.target.classList.contains("modal-controller")) {
-//       const modalId = event.target.dataset?.modalTarget;
-//       if (!modalId) return;
-//       const modal = document.getElementById(modalId);
-//       showModal(modal);
-//     } else if (event.target.classList.contains("modal-dismiss")) {
-//       const modalId = event.target.dataset?.modalTarget;
-//       if (!modalId) return;
-//       const modal = document.getElementById(modalId);
-//       hideModal(modal);
-//     } else if (event.target.classList.contains("modal-visible")) {
-//       const modal = document.getElementById(event.target.id);
-//       hideModal(modal);
-//     }
-//   },
-//   true
-// );
+modalDismiss.forEach((element) => {
+  element.addEventListener("click", (_) => {
+    const modalId = element.dataset?.modalTarget;
+    if (!modalId) return;
+    const modal = document.getElementById(modalId);
+    hideModal(modal);
+  });
+});
+modal.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    if (event.target.classList.contains("modal-visible")) {
+      const modal = document.getElementById(event.target.id);
+      hideModal(modal);
+    }
+  });
+}, false);
 
 const showModal = (modal) => {
+  body.style.overflow = "hidden";
   modal.classList.add("modal-visible");
   modal.style.display = "flex";
 };
 
 const hideModal = (modal) => {
+  body.style.overflow = "unset";
   modal.classList.remove("modal-visible");
   modal.style.display = "none";
 };
